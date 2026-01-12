19169cec25287816effe4b24b1b99f3104a6f28c
ragnikumari84123-sketch <ragnikumari84123@gmail.com>
2026-01-13 00:22:23 +0530

    Add vehicle lookup API endpoint

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from vehicle_lookup import vehicle
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Server is running 🚀"
+    return "API is running 🚀"
+
+@app.route("/vehicle")
+def get_vehicle():
+    number = request.args.get("number")
+    if not number:
+        return {"error": "number missing"}
+
+    data = vehicle(number)
+    return jsonify(data)
